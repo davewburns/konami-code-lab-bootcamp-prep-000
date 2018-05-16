c24d5cf6fd106bbd38efd4198ea90ad89aedb5ab
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-16 20:19:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,26 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  
+   // function onKeyDownHandler(e) {
+    document.body.addEventListener("keydown", (e) => {
+    const key = e.key;
+    
+    
+    if (key === codes[index]) {
+      index++;
+      
+        if (index === codes.length) {
+          window.alert('Congratulations! You know the Konami code!');
+          
+          index = 0;
+        }
+    } else {
+      index = 0;
+    }
+  }
+
 }
+  
+
